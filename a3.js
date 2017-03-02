a74d1cfffaedb019e6807cbdb242263fcc8711cc
Kai Yoshino <kaiyosh@uw.edu>
2017-03-02 09:21:13 -0800

    labels

--- a3.js
+++ a3.js
@@ -40,6 +40,7 @@ d3.csv("assets.csv", type, function(error, data) {
 		.attr("id", function(d) { return d.data.tick })
 		.attr("class", "arc")
 		.on("click", function(d) {
+			chart1.selectAll(".arc").classed("clicked", false)
 			toggleTicker(this)
 		});
 
@@ -69,7 +70,6 @@ function toggleTicker(ticker) {
 		  return d;
 		}, function(error, data) {
 		  if (error) throw error;
-		  	console.log(ticker.childNodes[0].style.fill)
 		  	focus.select("path.line").remove();
 
 			// add line
@@ -92,12 +92,17 @@ function toggleTicker(ticker) {
 
 // add line chart area to canvas
 var	chart2 = d3.select("body")
-	.attr("class", "chart2")
 	.append("svg")
+		.attr("id", "chart2")
+		.attr("class", "chart2")
 		.attr("width", width + margin.left * 2 + margin.right)
 		.attr("height", height + margin.top + margin.bottom * 2)
 	.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// // tool tip div
+// var tooltip = d3.select("body").append("div")
+//     .attr("class", "tooltip");
 
 var x = d3.scaleTime().range([0, width]),
 	x2 = d3.scaleTime().range([0, width]),
@@ -105,10 +110,10 @@ var x = d3.scaleTime().range([0, width]),
 	y2 = d3.scaleLinear().range([height2, 0]);
 
 var parseTime = d3.timeParse("%Y-%m-%d");
+	bisectDate = d3.bisector(function(d) { return d.aapl_date; }).left;
 	
 var xAxis = d3.axisBottom(x),
 	xAxis2 = d3.axisBottom(x2);
-
 
 var brush = d3.brushX()
 		.extent([[0, 0], [width, height2]])
@@ -121,15 +126,6 @@ var zoom = d3.zoom()
         .extent([[0, 0], [width, height]])
         .on("zoom", zoomed);
 
-
-
-// adds an area to axis 2 - not sure if I want this
-
-// var area2 = d3.area()
-//     .curve(d3.curveMonotoneX)
-//     .x(function(d) { return x2(d.date); })
-//     .y0(height2)
-//     .y1(function(d) { return y2(d.adj_close); });
 
 // focus area
 var focus = chart2.append("g")
@@ -148,99 +144,16 @@ chart2.append("defs").append("clipPath")
 		.attr("width", width)
 		.attr("height", height);
 
-
+// portfolio lines
 var portLine = d3.line()
     .x(function(d) { return x(d.aapl_date); })
     .y(function(d) { return y(d.port_close); });
 
-
+// asset lines
 var line = d3.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.adj_close); });
 
-// get line chart data
-d3.csv("./data/aapl.csv", function(d) {
-	d.date = parseTime(d.date);
-	d.adj_close = +d.adj_close;
-	return d;
-}, function(error, data) {
-	if (error) throw error;
-
-	// scale range of data
-	x.domain(d3.extent(data, function(d) { return d.date; }));
-	y.domain([0, 1100]);
-	x2.domain(x.domain());
-	y2.domain(y.domain());
-
-	// add x axis
- 	focus.append("g")
-		.attr("class", "axis axis--x")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
-
-	// y axis
-	focus.append("g")
-		.call(d3.axisLeft(y))
-		.append("text")
-		.attr("fill", "#000")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", "0.71em")
-		.attr("text-anchor", "end")
-		.text("Price ($)");
-
-	// add area for x axis 2
-	// context.append("path")
-	// 	.datum(data)
-	// 	.attr("class", "area")
-	// 	.attr("d", area2);
-
-	// x axis 2
-	context.append("g")
-		.attr("class", "axis axis--x")
-		.attr("transform", "translate(0," + height2 + ")")
-		.call(xAxis2);
-
-
-	// add brush area
-	context.append("g")
-		.attr("class", "brush")
-		.call(brush)
-		.call(brush.move, x.range());
-
-	chart2.append("rect")
-		.attr("class", "zoom")
-		.attr("width", width)
-		.attr("height", height)
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-		.call(zoom);
-
-});
-
-// brush function - big thanks to mike bostocks tutorials for this function
-function brushed() {
-	if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return;
-	var s = d3.event.selection || x2.range();
-	x.domain(s.map(x2.invert, x2));
-	chart2.select(".line").attr("d", line);
-	chart2.select(".portLine").attr("d", portLine);
-	chart2.select(".axis--x").call(xAxis);
-	chart2.select(".zoom").call(zoom.transform, d3.zoomIdentity
-		.scale(width / (s[1] - s[0]))
-		.translate(-s[0], 0));
-}
-
-
-// zoom function - again, thank you mike bostock!
-function zoomed() {
-    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return;
-    var t = d3.event.transform;
-    x.domain(t.rescaleX(x2).domain());
-    chart2.select(".line").attr("d", line);
-    chart2.select(".portLine").attr("d", portLine);
-    chart2.select(".axis--x").call(xAxis);
-    context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
-}
 
 // add specific stock lines
 d3.csv("./data/data.csv", function(d) {
@@ -263,9 +176,107 @@ d3.csv("./data/data.csv", function(d) {
 		.attr("stroke-linecap", "round")
 		.attr("stroke-width", 1.5)
 		.attr("d", portLine);
+
+	// scale range of data
+	x.domain(d3.extent(data, function(d) { return d.aapl_date; }));
+	y.domain([0, 1200]);
+	x2.domain(x.domain());
+	y2.domain(y.domain());
+
+	// add x axis
+ 	focus.append("g")
+		.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+	// y axis
+	focus.append("g")
+		.call(d3.axisLeft(y))
+		.append("text")
+		.attr("fill", "#000")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", "0.71em")
+		.attr("text-anchor", "end")
+		.text("Value ($)");
+
+	// x axis 2
+	context.append("g")
+		.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + height2 + ")")
+		.call(xAxis2);
+
+	var tooltip = chart2.append("g")
+		.attr("class", "tooltip")
+		.style("display", "none");
+
+	tooltip.append("text")
+		.attr("x", 10)
+		.attr("dy", ".35em");
+
+    tooltip.append("circle")
+        .attr("r", 7.5);
+
+	// add brush area
+	context.append("g")
+		.attr("class", "brush")
+		.call(brush)
+		.call(brush.move, x.range());
+
+	chart2.append("rect")
+		.attr("class", "zoom")
+		.attr("width", width)
+		.attr("height", height)
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		.on("mouseover", function() { tooltip.style("display", null); })
+		.on("mouseout", function() { tooltip.style("display", "none"); })
+		.on("mousemove", mousemove)
+		.call(zoom);
+
+
+
+
+	function mousemove() {
+		var x0 = x.invert(d3.mouse(this)[0]),
+			i = bisectDate(data, x0, 1),
+			d0 = data[i - 1],
+			d1 = data[i],
+			d = x0 - d0.aapl_date > d1.aapl_date - x0 ? d1 : d0;
+		tooltip.attr("transform", "translate(" + (x(d.aapl_date) + margin.left) + "," + (y(d.port_close) + margin.top)+ ")");
+		tooltip.select("text").text(d.port_close);
+	}
+
 });
 
 
+
+// brush function - big thanks to mike bostocks tutorials for this function
+function brushed() {
+	if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return;
+	var s = d3.event.selection || x2.range();
+	x.domain(s.map(x2.invert, x2));
+	focus.select(".grid")
+	focus.select(".line").attr("d", line);
+	focus.select(".portLine").attr("d", portLine);
+	focus.select(".axis--x").call(xAxis);
+	focus.select(".zoom").call(zoom.transform, d3.zoomIdentity
+		.scale(width / (s[1] - s[0]))
+		.translate(-s[0], 0));
+
+}
+
+
+// zoom function - again, thank you mike bostock!
+function zoomed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return;
+    var t = d3.event.transform;
+    x.domain(t.rescaleX(x2).domain());
+    focus.select(".grid")
+    focus.select(".line").attr("d", line);
+    focus.select(".portLine").attr("d", portLine);
+    focus.select(".axis--x").call(xAxis);
+    focus.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+    }
 
 
 
