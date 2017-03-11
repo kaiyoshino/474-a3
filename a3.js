a1a3efe8c7ae79ea8215c0fba9c8b71a53c773a0
Kai Yoshino <kaiyosh@uw.edu>
2017-03-10 19:36:28 -0800

    updates

--- a3.js
+++ a3.js
@@ -1,12 +1,12 @@
 
 
 // define canvas variables
-var margin = {top: 20, right: 20, bottom: 80, left: 40};
-	margin2 = {top: 450, right: 20, bottom: 30, left: 40};
-	width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    height2 = 520 - margin2.top - margin2.bottom,
-    radius = Math.min(width, height) / 2;
+var margin = {top: 20, right: 20, bottom: 80, left: 20};
+	margin2 = {top: 350, right: 20, bottom: 30, left: 20};
+	width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom,
+    height2 = 410 - margin2.top - margin2.bottom,
+    radius = Math.min(width, height) / 1.6;
 
 // pie colors
 var color = d3.scaleOrdinal()
@@ -14,8 +14,8 @@ var color = d3.scaleOrdinal()
 
 // pie arcs
 var arc = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(radius - 70);
+    .outerRadius(radius - 5)
+    .innerRadius(radius - 50);
 
 //pie chart
 var pie = d3.pie()
@@ -25,10 +25,10 @@ var pie = d3.pie()
 // add doughnut chart to canvas
 var chart1 = d3.select("body").append("svg")
 	.attr("id", "chart1")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top)
+	.attr("width", width - 70)
+	.attr("height", height + margin.top + margin.bottom + 100)
 		.append("g")
-	.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+	.attr("transform", "translate(" + ((width / 2) - 60) + "," + (height - margin.top - margin.bottom + 20) + ")");
 
 
 // get pie chart assets
@@ -39,10 +39,7 @@ d3.csv("assets.csv", type, function(error, data) {
 		.data(pie(data))
 		.enter().append("g")
 		.attr("id", function(d) { return d.data.tick })
-		.attr("class", "arc")
-		.on("click", function(d) {
-			toggleTicker(this)
-		});
+		.attr("class", "arc");
 
   	g.append("path")
       	.attr("d", arc)
@@ -59,40 +56,6 @@ function type(d) {
   	return d;
 }
 
-// toggle specific stock lines on chart2 by clicking arcs
-function toggleTicker(ticker) {
-	if (ticker.classList.contains("clicked")) {
-		ticker.classList.remove("clicked")
-		chart2.selectAll("path#line-" + ticker.getAttribute("id") + "").remove()
-	} else {
-		elements = document.getElementById("chart1").childNodes[0].childNodes
-		for (var i = 0; i < elements.length; i++) {
-			elements[i].classList.remove('clicked');
-		}
-		d3.csv("./data/" + ticker.getAttribute("id").toLowerCase()  + ".csv", function(d) {
-		  d.date = parseTime(d.date);
-		  d.adj_close = +d.adj_close;
-		  return d;
-		}, function(error, data) {
-		  if (error) throw error;
-		  	focus.select("path.line").remove();
-
-			// add line
-			focus.append("path")
-				.datum(data)
-				.attr("class", "line")
-				.attr("id", "line-" + ticker.getAttribute("id"))
-				.attr("fill", "none")
-				.attr("stroke", ticker.childNodes[0].style.fill) // get color by id
-				.attr("stroke-linejoin", "round")
-				.attr("stroke-linecap", "round")
-				.attr("stroke-width", 3)
-				.attr("d", line);
-		});
-		ticker.classList.add("clicked");
-	}
-}
-
 
 
 // add line chart area to canvas
@@ -100,14 +63,10 @@ var	chart2 = d3.select("body")
 	.append("svg")
 		.attr("id", "chart2")
 		.attr("class", "chart2")
-		.attr("width", width + margin.left * 2 + margin.right)
-		.attr("height", height + margin.top + margin.bottom * 2.5)
+		.attr("width", width + margin.left * 2 + margin.right + 90 )
+		.attr("height", height + margin.top + margin.bottom + 110)
 	.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// // tool tip div
-// var tooltip = d3.select("body").append("div")
-//     .attr("class", "tooltip");
+		.attr("transform", "translate(" + 20 + "," + margin.top + ")");
 
 var x = d3.scaleTime().range([0, width]),
 	x2 = d3.scaleTime().range([0, width]),
@@ -131,11 +90,20 @@ var zoom = d3.zoom()
         .extent([[0, 0], [width, height]])
         .on("zoom", zoomed);
 
+var clip = chart2.append("svg:clipPath")
+    .attr("id", "clip")
+    .append("svg:rect")
+    .attr("id", "clip-rect")
+    .attr("x", "0")
+    .attr("y", "0")
+    .attr('width', width)
+    .attr('height', height);
 
 // focus area
 var focus = chart2.append("g")
         .attr("class", "focus")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
 // context area
 var context = chart2.append("g")
@@ -152,7 +120,10 @@ chart2.append("defs").append("clipPath")
 // portfolio lines
 var portLine = d3.line()
     .x(function(d) { return x(d.aapl_date); })
-    .y(function(d) { return y(d.port_close); });
+    .y(function(d) { 
+    	return y(d.port_close); });
+
+var asset_line;
 
 // asset lines
 var line = d3.line()
@@ -174,6 +145,7 @@ d3.csv("./data/data.csv", function(d) {
 	// add portfolio value line
   	focus.append("path")
 		.datum(data)
+		.attr("clip-path", "url(#clip)")
 		.attr("class", "portLine")
 		.attr("fill", "none")
 		.attr("stroke", "steelblue")
@@ -215,12 +187,51 @@ d3.csv("./data/data.csv", function(d) {
 		.attr("class", "tooltip")
 		.style("display", "none");
 
+	tooltip.append("rect")
+		.attr("width", 85)
+		.attr("height", 90)
+		.attr("class", "tooltip-rect")
+
+	focus.append("line")
+        .attr("class", "x")
+        .style("stroke", "blue")
+        .style("stroke-dasharray", "3,3")
+        .style("opacity", 0.5)
+        .attr("y1", 0)
+        .attr("y2", height);
+
+
 	tooltip.append("text")
-		.attr("x", 10)
+		.attr("x", 5)
+		.attr("y", 10)
+		.attr("class", "tooltip-rect-text-1")
+		.attr("dy", ".35em")
+	tooltip.append("text")
+		.attr("x", 5)
+		.attr("y", 24)
+		.attr("class", "tooltip-rect-text-2")
+		.attr("dy", ".35em");
+	tooltip.append("text")
+		.attr("x", 5)
+		.attr("y", 38)
+		.attr("class", "tooltip-rect-text-3")
+		.attr("dy", ".35em");
+	tooltip.append("text")
+		.attr("x", 5)
+		.attr("y", 52)
+		.attr("class", "tooltip-rect-text-4")
+		.attr("dy", ".35em");	
+	tooltip.append("text")
+		.attr("x", 5)
+		.attr("y", 66)
+		.attr("class", "tooltip-rect-text-5")
+		.attr("dy", ".35em");
+	tooltip.append("text")
+		.attr("x", 5)
+		.attr("y", 80)
+		.attr("class", "tooltip-rect-text-6")
 		.attr("dy", ".35em");
 
-    tooltip.append("circle")
-        .attr("r", 7.5);
 
 	// add brush area
 	context.append("g")
@@ -233,8 +244,14 @@ d3.csv("./data/data.csv", function(d) {
 		.attr("width", width)
 		.attr("height", height)
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-		.on("mouseover", function() { tooltip.style("display", null); })
-		.on("mouseout", function() { tooltip.style("display", "none"); })
+		.on("mouseover", function() { 
+			tooltip.style("display", null); 
+			focus.select(".x").style("display", null);
+		})
+		.on("mouseout", function() { 
+			tooltip.style("display", "none");
+			focus.select(".x").style("display", "none");
+		})
 		.on("mousemove", mousemove)
 		.call(zoom);
 
@@ -247,8 +264,71 @@ d3.csv("./data/data.csv", function(d) {
 			d0 = data[i - 1],
 			d1 = data[i],
 			d = x0 - d0.aapl_date > d1.aapl_date - x0 ? d1 : d0;
-		tooltip.attr("transform", "translate(" + (x(d.aapl_date) + margin.left) + "," + (y(d.port_close) + margin.top)+ ")");
-		tooltip.select("text").text(d.port_close);
+		tooltip.attr("transform", "translate(" + (x(d.aapl_date) + margin.left + 5) + "," + (d3.event.pageY - 215) + ")");
+		tooltip.select(".tooltip-rect-text-1").text("Portfolio: " + parseFloat(d.port_close).toFixed(2));
+		tooltip.select(".tooltip-rect-text-2").text("SBUX: " + parseFloat(d.sbux_close).toFixed(2));
+		tooltip.select(".tooltip-rect-text-3").text("GOOGL: " + parseFloat(d.googl_close).toFixed(2));
+		tooltip.select(".tooltip-rect-text-4").text("NDAQ: " + parseFloat(d.ndaq_close).toFixed(2));
+		tooltip.select(".tooltip-rect-text-5").text("GE: " + parseFloat(d.ge_close).toFixed(2));
+		tooltip.select(".tooltip-rect-text-6").text("AAPL: " + parseFloat(d.aapl_close).toFixed(2));
+		focus.select(".x")
+		    .attr("transform",
+		          "translate(" + x(d.aapl_date) + "," +
+		                         0 + ")")
+		               .attr("y2", height);
+	}
+
+  	var g = chart1.selectAll(".arc")
+		.on("click", function(d) {
+			toggleTicker(this)
+	});
+
+
+
+	// toggle specific stock lines on chart2 by clicking arcs
+	function toggleTicker(ticker) {
+		// asset lines
+		asset_line = d3.line()
+		    .x(function(d) { return x(d.aapl_date); })
+		    .y(function(d) { 
+		    	var asset_data = {
+					googl: d.googl_close,
+					sbux: d.sbux_close,
+					ndaq: d.ndaq_close,
+					ge: d.ge_close,
+					aapl: d.aapl_close
+				}
+		    	return y(asset_data[ticker.getAttribute("id").toLowerCase()]); 
+		    });
+
+		if (ticker.classList.contains("clicked")) {
+			ticker.classList.remove("clicked")
+			chart2.selectAll("path#line-" + ticker.getAttribute("id") + "").remove()
+		} else {
+			elements = document.getElementById("chart1").childNodes[0].childNodes
+			for (var i = 0; i < elements.length; i++) {
+				elements[i].classList.remove('clicked');
+			}
+
+
+
+			focus.select("path.line").remove();
+
+			// add line
+			focus.append("path")
+				.attr("clip-path", "url(#clip)")
+				.datum(data)
+				.attr("class", "line")
+				.attr("id", "line-" + ticker.getAttribute("id"))
+				.attr("fill", "none")
+				.attr("stroke", ticker.childNodes[0].style.fill) // get color by id
+				.attr("stroke-linejoin", "round")
+				.attr("stroke-linecap", "round")
+				.attr("stroke-width", 3)
+				.attr("d", asset_line);
+
+			ticker.classList.add("clicked");
+		}
 	}
 
 });
@@ -261,26 +341,26 @@ function brushed() {
 	var s = d3.event.selection || x2.range();
 	x.domain(s.map(x2.invert, x2));
 	focus.select(".grid")
-	focus.select(".line").attr("d", line);
+	focus.selectAll(".line").attr("d", asset_line);
 	focus.select(".portLine").attr("d", portLine);
 	focus.select(".axis--x").call(xAxis);
-	focus.select(".zoom").call(zoom.transform, d3.zoomIdentity
+	chart2.select(".zoom").call(zoom.transform, d3.zoomIdentity
 		.scale(width / (s[1] - s[0]))
 		.translate(-s[0], 0));
 
 }
 
 
-// zoom function - again, thank you mike bostock!
+// zoom function
 function zoomed() {
     if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return;
     var t = d3.event.transform;
     x.domain(t.rescaleX(x2).domain());
     focus.select(".grid")
-    focus.select(".line").attr("d", line);
+    focus.selectAll(".line").attr("d", asset_line);
     focus.select(".portLine").attr("d", portLine);
     focus.select(".axis--x").call(xAxis);
-    focus.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+    context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
     }
 
 
